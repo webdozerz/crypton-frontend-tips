cbfb5f4c32ed2416e1ab45fd7cb573c479138baf
iliyaandrushov <webdozerz@gmail.com>
2021-07-14 20:14:25 +0300

    Updates

---
+++ _nuxt/eefee37.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{354:function(e,n,t){"use strict";t.r(n);var l={name:"BemPage"},o=t(27),component=Object(o.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("Bem")}),[],!1,null,null,null);n.default=component.exports;installComponents(component,{Bem:t(205).default})}}]);
