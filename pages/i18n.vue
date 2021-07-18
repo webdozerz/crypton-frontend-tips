9ca988671b09ee4e8ee8b09f3cc47db43a900127
iliyaandrushov <webdozerz@gmail.com>
2021-07-18 20:49:35 +0300

    [update] locales

--- pages/i18n.vue
+++ pages/i18n.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <h4>Локализация</h4>
-    <p class="mt-3">
-      Начнем с общего требования
-    </p>
+    <h4>Общее требование</h4>
     <v-alert
       type="info"
       dense
@@ -12,7 +9,79 @@
     >
       Всегда оборачивайте текст на странице в переменные локализации! Даже если дальнейшая локализация на проекте не предусмотрена.
     </v-alert>
-    Приведем пример <code>.json</code> файла с локализацией:
+    <h4>
+      Установка и настройка библиотеки nuxt-i18n
+    </h4>
+    <p class="mt-2">
+      Для реализации локализации проекта необходимо установить библиотеку <a
+        href="https://i18n.nuxtjs.org/"
+        target="_blank"
+      >
+        nuxt-i18n
+      </a> командой <code>npm install nuxt-i18n</code>.
+    </p>
+    <p>
+      После этого в файле <code>nuxt.config.js</code> в секции <code>modules</code> необходимо добавить следующее:
+    </p>
+    <div
+      v-highlight
+      class="code"
+    >
+      <pre class="language-javascript">
+        <code>
+          modules: [
+            'nuxt-i18n',
+          ],
+        </code>
+      </pre>
+    </div>
+    <p>
+      Далее создайте в корне проекта папку <code>locales</code>. Внутри нее создайте <code>.json</code> файл с локализацией. Например, <code>localeEn</code>.
+    </p>
+    <p>
+      В самом начале файла <code>nuxt.config.js</code>, до <code>export default {}</code>, импортируйте <code>.json</code> файл с локализацией.
+    </p>
+    <div
+      v-highlight
+      class="code"
+    >
+      <pre class="language-javascript">
+        <code>
+          import localeEn from './locales/en.json';
+        </code>
+      </pre>
+    </div>
+    <p>
+      После закрытия секции <code>modules</code> добавьте следующее:
+    </p>
+    <div
+      v-highlight
+      class="code"
+    >
+      <pre class="language-javascript">
+        <code>
+           i18n: {
+            locales: ['en'],
+            defaultLocale: 'en',
+            strategy: 'no_prefix',
+            vueI18n: {
+              messages: {
+                en: localeEn,
+              },
+            },
+            detectBrowserLanguage: {
+              useCookie: true,
+              cookieKey: 'i18n_redirected',
+              alwaysRedirect: true,
+            },
+          },
+        </code>
+      </pre>
+    </div>
+    <h4>Пример использования</h4>
+    <p class="mt-2">
+      Приведем пример <code>.json</code> файла с локализацией:
+    </p>
     <div
       v-highlight
       class="code"
@@ -20,10 +89,12 @@
       <pre class="language-javascript">
             <code>
               {
-                "keyName": "value",
-                "parent": {
-                  "child": "value",
-                  "childSecond": "value"
+                "signIn": "Sign In",
+                "header": {
+                  "title": "My blockchain website",
+                  "links": {
+                    "about": "About"
+                  }
                 }
               }
             </code>
@@ -41,10 +112,39 @@
         class="language-javascript"
       >
             <code>
-              {{ $t('parent.child') }}
+              {{ $t('header.title') }}
             </code>
         </pre>
     </div>
+    <p>
+      Для переключения языка необходимо вызвать следующую функцию:
+    </p>
+    <div
+      v-highlight
+      class="code"
+    >
+      <pre
+        v-pre
+        class="language-javascript"
+      >
+        <code>
+          changeLanguage(locale) {
+            this.$i18n.setLocale(locale);
+          }
+        </code>
+        </pre>
+    </div>
+    <p>
+      Где вместо locale необходимо передать код локализации. Например, <code>this.$i18n.setLocale('en')</code> или <code>this.$i18n.setLocale('ru')</code>.
+    </p>
+    <v-alert
+      type="info"
+      dense
+      outlined
+      class="mt-2 code"
+    >
+      Библиотека nuxt-i18n сама применит нужную локализацию на сайте в соответствии с текущим языком браузера.
+    </v-alert>
   </div>
 </template>
 
